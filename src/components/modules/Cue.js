70808929d16b540bfb444ef31303ec415cdcda19
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-10-10 13:48:18 +0330

    cue components designed

--- src/components/modules/Cue.js
+++ src/components/modules/Cue.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import styles from './Cue.module.css'
+import React from "react";
+import styles from "./Cue.module.css";
 
 function Cue() {
   return (
     <div className={styles.container}>
-        <div className={styles.cue}>
-            <div className={styles.badge}>
-                <p>Phoenix</p>
-                <img  />
-            </div>
+      <div className={styles.cue}>
+        <div className={styles.badge}>
+          <p>Phoenix</p>
+          <img src="images/badge.png" />
         </div>
-        <div className={styles.info}></div>
+        <div className={styles.cueContianer}>
+          <img src="/images/cue.png" />
+        </div>
+      </div>
+      <div className={styles.info}></div>
     </div>
-  )
+  );
 }
 
-export default Cue
+export default Cue;
